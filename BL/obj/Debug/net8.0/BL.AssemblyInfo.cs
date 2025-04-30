1a6701bb1a6e8cdbf22cd1e8204b647524ea7215
racheliGerlitz <r0583223328@gmail.com>
2025-04-30 17:43:42 +0300

    העלאת פרויקט חדש

--- BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
+++ BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68aca9d1a437b9ef7060f57d8d35a7e5c27101c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
